0a18bdba581671018135abe6671c8a71338c4656
Bo YU <tsu.yubo@gmail.com>
2023-12-26 19:28:15 +0800

    inew test
    Signed-off-by: Bo YU <tsu.yubo@gmail.com>

--- rootfs/setup_rootfs.sh
+++ rootfs/setup_rootfs.sh
@@ -9,10 +9,12 @@ export LC_ALL=C LANGUAGE=C LANG=C
 /var/lib/dpkg/info/sgml-base.preinst install
 
 mkdir -p /etc/sgml
-dpkg --configure -a
 mount proc -t proc /proc
+mount -B sys /sys
+mount -B run /run
+mount -B dev /dev
+#mount devpts -t devpts /dev/pts
 dpkg --configure -a
-umount /proc
 # Needed because we get permissions problems for some reason
 chmod 0666 /dev/null
 
@@ -40,7 +42,9 @@ cat > /etc/fstab <<EOF
 /dev/mmcblk1p2 /boot/efi      vfat    umask=0077      0       1
 EOF
 
-apt install -f /tmp/*.deb
+#apt install -f /tmp/*.deb
+
+mv /tmp/*.deb /boot
 
 # change device tree
 echo "===== ln -s dtb files ====="
@@ -57,24 +61,29 @@ fi
 
 kernel_image=${lib_dir##*/}
 
-cd ${lib_dir}/starfive/
-ln -s jh7110-starfive-visionfive-2-v1.2a.dtb jh7110-visionfive-v2.dtb
-cd - 
-
-#update-initramfs -c -k all
-# update u-boot
 
 # cp your latest dtb file,e.g, cp /usr/lib/linux-image-xx-riscv64
 # need you confirm it here
-apt install u-boot-menu
 cat <<EOF >> /etc/default/u-boot
 U_BOOT_PARAMETERS="rw console=tty0 console=ttyS0,115200 earlycon rootwait stmmaceth=chain_mode:1 selinux=0"
 EOF
 
-dpkg-reconfigure ${kernel_image}
+cat /boot/extlinux/extlinux.conf
+
+#dpkg-reconfigure ${kernel_image}
+
+# update u-boot
+#update-initramfs -c -k all
+
+u-boot-update
 
 # boot from sd card
-sed -i -e 's|append |append root=/dev/mmcblk1p3 |' /boot/extlinux/extlinux.conf
+cat <<EOF >> /boot/scripts.txt
+sed -i -e 's|root=[^ ]*|root=/dev/mmcblk1p3|' /boot/extlinux/extlinux.conf
+cd ${lib_dir}/starfive/
+ln -s jh7110-starfive-visionfive-2-v1.2a.dtb jh7110-visionfive-v2.dtb
+cd - 
+EOF
 
 # double check
 cat /boot/extlinux/extlinux.conf
@@ -88,8 +97,6 @@ kernel_comp_addr_r=0xb0000000
 kernel_comp_size=0x10000000
 EOF
 
-
-
 # Set hostname
 echo vf2 > /etc/hostname
 
@@ -100,8 +107,6 @@ EOF
 
 # Add needed modules in initrd
 #echo "nvme" >> /etc/initramfs-tools/modules
-
-
 
 # 
 # Enable system services
@@ -120,6 +125,11 @@ EOF
 # Clean apt cache on the system
 #
 apt-get clean
+
+umount /proc
+umount /sys
+umount /run
+umount /dev
 
 # set the time immediately at startup
 #sed -i 's/^DAEMON_OPTS="/DAEMON_OPTS="-s /' /etc/default/openntpd
